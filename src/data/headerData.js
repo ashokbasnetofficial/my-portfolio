c51260c1431f93da2582f30ed4334b7bd68c3abc
Ashok Basnet <87795084+ashokbasnetofficial@users.noreply.github.com>
2023-06-03 16:29:31 +0545

    update profile picture

--- src/data/headerData.js
+++ src/data/headerData.js
@@ -1,9 +1,10 @@
 
-export const headerData = {
+import logo from '../../src/assets/logo/ashok basnet.JPG'
+ export const headerData = {
     name: 'ASHOK BASNET',
     title: "Hello! I am",
     desciption: "Hello! I am Ashok Basnet, a professional and passionate programmer in my daily life. A quick learner with a self-learning attitude. I love to learn and explore new technologies and am Passionate about Problem Solving. ",
-    image: 'https://scontent.fbir5-1.fna.fbcdn.net/v/t39.30808-6/326710722_569784041719153_1075433510947550451_n.jpg?_nc_cat=106&ccb=1-7&_nc_sid=09cbfe&_nc_ohc=D7gx3oVIdXIAX9cWBYL&_nc_ht=scontent.fbir5-1.fna&oh=00_AfDoD60R-1WYUAyHbo-40a7YoSocmztfHJh5OFu2NDVnSg&oe=647A8495',
-    imagebw: 'https://scontent.fbir5-1.fna.fbcdn.net/v/t39.30808-6/326710722_569784041719153_1075433510947550451_n.jpg?_nc_cat=106&ccb=1-7&_nc_sid=09cbfe&_nc_ohc=D7gx3oVIdXIAX9cWBYL&_nc_ht=scontent.fbir5-1.fna&oh=00_AfDoD60R-1WYUAyHbo-40a7YoSocmztfHJh5OFu2NDVnSg&oe=647A8495',
-    resumePdf: 'https://docs.google.com/document/d/1MkkoRX98FS47CaEyeodyPzi4OkW8SH7Gv55f4MQkHV8/edit?usp=sharing'
+    image:logo,
+    imagebw: logo,
+    resumePdf: ''
 }
