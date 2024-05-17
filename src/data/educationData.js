2b2ad8b9d65e2efd5d18ec4287a16cf720352ec4
Ashok Basnet <87795084+ashokbasnetofficial@users.noreply.github.com>
2024-05-17 22:16:40 +0545

    Update educationData.js
    add education data

--- src/data/educationData.js
+++ src/data/educationData.js
@@ -1,20 +1,27 @@
 export const educationData = [
     {
         id: 1,
-        institution: 'Mechi Multiple Campus',
-        course: 'Bsc.CSIT Bachelor Level',
-        startYear: '2018',
-        endYear: 'Present'
+        institution: 'Central Department of CSIT,TU',
+        course: 'Msc. CSIT Master Degree',
+        startYear: '2023',
+        endYear: '2025'
     },
     {
         id: 2,
+        institution: 'Mechi Multiple Campus',
+        course: 'Bsc.CSIT Bachelor Level',
+        startYear: '2018',
+        endYear: '2022'
+    },
+    {
+        id: 3,
         institution: 'Devi Secondary School',
         course: 'Higher Secondary',
         startYear: '2016',
         endYear: '2018'
     },
     {
-        id: 3,
+        id: 4,
         institution: 'Shree Bagawati Ma Vi',
         course: 'Secondary School',
         startYear: '2006',
